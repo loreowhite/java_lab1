144461b4ca3b1cda3102a8b00f335f28fcb574db
Ilya Filippov <ilyafilippov@MacBook-Pro-Ilya.local>
2025-05-24 00:04:40 +0500

    модель и отображение списка постов

--- src/main/java/com/example/demo/controller/PostsViewController.java
+++ src/main/java/com/example/demo/controller/PostsViewController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PostsViewController {
+    @Autowired
+    private PostService postsService;
+
     @ResponseBody
     @RequestMapping(path = "/post/{id}", method = RequestMethod.GET)
     public String single(@PathVariable("id") Long id) {
@@ -17,6 +22,7 @@ public class PostsViewController {
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String list(Model model) {
         model.addAttribute("appName", "Мое супер приложение");
+        model.addAttribute("posts", postsService.listAllPosts());
         return "list";
     }
 }
