af36e7016caee3127ecc035d8abe9293452a1572
Ilya Filippov <ilyafilippov@MacBook-Pro-Ilya.local>
2025-05-24 00:20:59 +0500

    Bootstrap верстка

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -1,17 +1,23 @@
 package com.example.demo.model;
 
+import java.util.Date;
 import java.util.List;
 
 public class Post {
     private String text;
+    private Date creationDate;
     private Integer likes;
 
     public Post(String text){
         this.text = text;
+        this.creationDate = new Date();
         this.likes = 0;
     }
     public String getText() {
         return text;
+    }
+    public Date getCreationDate() {
+        return creationDate;
     }
     public Integer getLikes() {
         return likes;
